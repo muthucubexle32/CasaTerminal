e20a88d15763eafa69874d3790c0fddcaacb46b3
Your GitHub Username <your-github-email@example.com>
2026-05-02 18:22:14 +0530

    pending

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,9 +4,7 @@ import CoreServices from '../components/sections/CoreServices';
 import ServiceCarousel from '../components/sections/ServiceSwipe';
 import WhyChooseUs from '../components/sections/WhyChooseUs';
 import LocationPrompt from '../components/LocationPrompt';
-import NearbyProducts from '../components/NearbyProducts';
-import NearbyContractors from '../components/NearbyContractors';
-import NearbyRentals from '../components/NearbyRentals';
+
 
 const LandingPage: React.FC = () => {
   return (
@@ -15,14 +13,6 @@ const LandingPage: React.FC = () => {
       <HeroSection />
       <CoreServices />
       <ServiceCarousel />
-      
-      {/* Nearby Sections */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <NearbyProducts />
-        <NearbyContractors />
-        <NearbyRentals />
-      </div>
-      
       <WhyChooseUs />
     </>
   );
